7fde029d3af76ee6ba306d95359d4aa1da1ea1c3
emrah <emrah.com@gmail.com>
2019-10-30 16:07:12 +0300

    the config file for the flask application

--- machines/eb-audition/home/eb-user/application/audition/run.py
+++ machines/eb-audition/home/eb-user/application/audition/run.py
@@ -1,5 +1,5 @@
 from app import app
-from app.models import *
+#from app.models import *
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
---
+++ machines/eb-audition/home/eb-user/application/config/audition.py
@@ -0,0 +1,9 @@
+# -----------------------------------------------------------------------------
+# AUDITION.PY
+#
+# This is a Python script used as the config file. Link this file to
+# the application directory using the name 'config.py'
+#
+#     cd audition
+#     ln -s ../config/audition.py config.py
+# -----------------------------------------------------------------------------

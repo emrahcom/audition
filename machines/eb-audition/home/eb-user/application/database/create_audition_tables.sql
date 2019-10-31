0c44049b676485ad64c698443145f675a56974aa
emrah <emrah.com@gmail.com>
2019-10-31 22:35:13 +0300

    added the tables: employer, job

--- machines/eb-audition/home/eb-user/application/database/create_audition_tables.sql
+++ machines/eb-audition/home/eb-user/application/database/create_audition_tables.sql
@@ -28,5 +28,41 @@ CREATE TABLE param (
 );
 ALTER TABLE param OWNER TO audition;
 -- ----------------------------------------------------------------------------
+-- EMPLOYER
+-- ----------------------------------------------------------------------------
+-- This table stores the employer base data.
+--
+-- id                   : the record id
+-- email                : the email address
+-- passwd               : the pasword hash
+-- active               : is the record active?
+-- ----------------------------------------------------------------------------
+CREATE TABLE employer (
+    "id" serial NOT NULL PRIMARY KEY,
+    "email" varchar(250) NOT NULL UNIQUE,
+    "passwd" varchar(250),
+    "active" boolean NOT NULL DEFAULT TRUE
+);
+CREATE INDEX employer_active ON employer("active");
+ALTER TABLE employer OWNER TO audition;
+-- ----------------------------------------------------------------------------
+-- JOB
+-- ----------------------------------------------------------------------------
+-- This table stores the job base data.
+--
+-- id                   : the record id
+-- employer             : the employer
+-- active               : is the record active?
+-- ----------------------------------------------------------------------------
+CREATE TABLE job (
+    "id" serial NOT NULL PRIMARY KEY,
+    "employer" integer NOT NULL REFERENCES employer("id")
+                                ON DELETE CASCADE,
+    "active" boolean NOT NULL DEFAULT TRUE
+);
+CREATE INDEX job_employer ON job("employer");
+CREATE INDEX job_active ON job("active");
+ALTER TABLE job OWNER TO audition;
+-- ----------------------------------------------------------------------------
 
 COMMIT;

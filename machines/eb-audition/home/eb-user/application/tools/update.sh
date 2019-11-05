12ec850a5330fae9160a12cbca77d136e23109b6
emrah <emrah.com@gmail.com>
2019-11-05 23:58:16 +0300

    the application update script

---
+++ machines/eb-audition/home/eb-user/application/tools/update.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+set -e
+
+# -----------------------------------------------------------------------------
+# UPDATE.SH
+# -----------------------------------------------------------------------------
+# Update the application from the repository.
+#
+# Usage:
+#     bash update.sh
+# -----------------------------------------------------------------------------
+APP_REPO="https://github.com/emrahcom/audition.git"
+APP_BRANCH="master"
+APP_TMP_DIR="/tmp/eb_tmp"
+APP_BASE="$APP_TMP_DIR/machines/eb-audition/home/eb-user/application"
+APP="audition"
+
+START_TIME=$(date +%s)
+DATE=$(date +"%Y%m%d%H%M%S")
+BASEDIR=$(pwd)
+PRODUCTION_BASE="$BASEDIR/../"
+
+# -----------------------------------------------------------------------------
+# CLONING THE GIT REPO
+# -----------------------------------------------------------------------------
+rm -rf $APP_TMP_DIR
+git clone --depth=1 -b $APP_BRANCH $APP_REPO $APP_TMP_DIR
+
+# -----------------------------------------------------------------------------
+# CHECK
+# -----------------------------------------------------------------------------
+# the application config
+echo "Checking the application config..."
+diff -I 'SECRET_KEY' $PRODUCTION_BASE/config/audition.conf \
+    $APP_BASE/config/audition.conf
+
+# the application globals
+echo "Checking the application globals..."
+diff -I '@eb-audition-db/audition' \
+    $PRODUCTION_BASE/config/audition-globals.py \
+    $APP_BASE/config/audition-globals.py
+
+# the application uwsgi ini
+echo "Checking the application UWSGI settings..."
+diff $PRODUCTION_BASE/uwsgi/audition-uwsgi.ini \
+    $APP_BASE/uwsgi/audition-uwsgi.ini
+
+# the application database
+echo "Checking the application database..."
+diff $PRODUCTION_BASE/database/ $APP_BASE/database/
+
+# the application tools
+echo "Checking the application tools..."
+diff $PRODUCTION_BASE/tools/ $APP_BASE/tools/
+
+# -----------------------------------------------------------------------------
+# UPDATE APPLICATION
+# -----------------------------------------------------------------------------
+if [ ! -d "$APP_BASE/$APP" ] 
+then
+    echo
+    echo "Warning: the application folder is missing."
+    echo "Interrupted..."
+    exit
+fi
+
+mkdir -p $PRODUCTION_BASE/$APP-backup
+mv $PRODUCTION_BASE/$APP $PRODUCTION_BASE/$APP-backup/$APP-$DATE
+mv $APP_BASE/$APP $PRODUCTION_BASE/
+
+# -----------------------------------------------------------------------------
+# REMINDER
+# -----------------------------------------------------------------------------
+echo
+echo "------------------------ REMINDER ------------------------"
+echo "Don't forget to restart UWSGI service."
+echo
+echo "  systemctl restart uwsgi.service"
+echo
+echo "----------------------------------------------------------"
+
+# -----------------------------------------------------------------------------
+# INSTALLATION DURATION
+# -----------------------------------------------------------------------------
+END_TIME=$(date +%s)
+DURATION=$(date -u -d "0 $END_TIME seconds - $START_TIME seconds" +"%H:%M:%S")
+
+echo
+echo Installation Duration: $DURATION

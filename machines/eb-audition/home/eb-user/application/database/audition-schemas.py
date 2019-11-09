bbe4788b0732a93e276b67525d34a0c945b91d79
emrah <emrah.com@gmail.com>
2019-11-09 20:29:39 +0300

    passwd schema

--- machines/eb-audition/home/eb-user/application/database/audition-schemas.py
+++ machines/eb-audition/home/eb-user/application/database/audition-schemas.py
@@ -10,9 +10,28 @@
 
 from schema import Schema, Regex, Use, And, Optional
 
-ID_SCH = Schema(And(Use(int), lambda n: n > 0))
+
+def check_passwd(passwd):
+    try:
+        if not (5 < len(passwd) < 32):
+            return False
+
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
 
 NON_EMPTY_DICT_SCH = Schema({str: object})
+ID_SCH = Schema(And(Use(int), lambda n: n > 0))
+PASSWD_SCH = Schema(And(str, check_passwd))
+
+EMPLOYER_CREATE_SCH = Schema({
+    'email': And(str, Use(str.lower),
+                 Regex(r'^[a-z0-9_.+-]+@[a-z0-9-]+\.[a-z0-9.-]+$')),
+    'passwd': PASSWD_SCH,
+    'active': bool
+}, ignore_extra_keys=True)
 
 EMPLOYER_UPDATE_SCH = Schema({
     Optional('email'): And(str, Use(str.lower),

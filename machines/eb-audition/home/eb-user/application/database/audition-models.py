3568972444f5034fa20c45241c1f286cc72e7b5e
emrah <emrah.com@gmail.com>
2020-02-16 20:16:41 +0300

    database: uodated the sqlalchemy model

--- machines/eb-audition/home/eb-user/application/database/audition-models.py
+++ machines/eb-audition/home/eb-user/application/database/audition-models.py
@@ -9,7 +9,8 @@
 # -----------------------------------------------------------------------------
 from app.globals import DB_URI
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, text
+from sqlalchemy import (Column, Integer, String, DateTime, Boolean, ForeignKey,
+                        text)
 from sqlalchemy.orm import sessionmaker, relationship, backref
 from sqlalchemy.pool import QueuePool
 from sqlalchemy.ext.declarative import declarative_base
@@ -42,14 +43,22 @@ class Employer(Table):
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False, unique=True)
     passwd = Column(String(250), nullable=False)
-    active = Column(Boolean, nullable=False, index=True,
-                    default=True, server_default=text('TRUE'))
+    name = Column(String(250), nullable=False, unique=True)
+    avatar = Column(String(250), nullable=False, default='',
+                    server_default=text(''))
+    active = Column(Boolean, nullable=False, index=True, default=True,
+                    server_default=text('TRUE'))
+    created_at = Column(DateTime(timezone=True), nullable=False, index=True,
+                        server_default=text('NOW()'))
 
     def to_dict(self):
         return {'id': self.id,
                 'email': self.email,
                 'passwd': self.passwd,
-                'active': self.active}
+                'name': self.name,
+                'avatar': self.avatar,
+                'active': self.active,
+                'created_at': self.created_at}
 
 
 class Job(Table):
@@ -63,12 +72,26 @@ class Job(Table):
                             cascade='all, delete-orphan',
                             passive_deletes=True))
     title = Column(String(250), nullable=False)
+    cost = Column(Integer, nullable=False, default=0, server_default=0)
+    status = Column(Integer, nullable=False, index=True, default=0,
+                  server_default=0)
+    hidden = Column(Boolean, nullable=False, index=True,
+                    default=False, server_default=text('FALSE'))
     active = Column(Boolean, nullable=False, index=True,
                     default=True, server_default=text('TRUE'))
+    created_at = Column(DateTime(timezone=True), nullable=False, index=True,
+                        server_default=text('NOW()'))
+    updated_at = Column(DateTime(timezone=True), nullable=False, index=True,
+                        server_default=text('NOW()'))
 
     def to_dict(self):
         return {'id': self.id,
                 'employer_id': self.employer_id,
                 'employer': self.employer.email,
                 'title': self.title,
-                'active': self.active}
+                'cost': self.cost,
+                'status': self.status,
+                'hidden': self.hidden,
+                'active': self.active,
+                'created_at': self.created_at,
+                'updated_at': self.updated_at}
